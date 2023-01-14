3bf5f58a096c3c15d3c5fca72cc07cda5c7430ea
Ilia <shishmagaevi@mail.ru>
2023-01-14 10:40:02 +0300

    add task07

---
+++ Task07/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+int div = number % 10;
+
+Console.WriteLine(div);
+}
+else 
+{
+    Console.WriteLine("Введите натуральное число");
+}
