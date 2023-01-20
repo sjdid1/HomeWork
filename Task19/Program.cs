ede1c4c875a1b36e160fee429f4ec78129b8cd9d
Ilia <shishmagaevi@mail.ru>
2023-01-20 18:36:54 +0300

    Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,7 +7,7 @@
 
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 9999)
+if (number < 9999 && -9999 < number)
 {
     Console.WriteLine("Введите пятизначное число");
     return;
