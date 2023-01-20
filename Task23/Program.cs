229f2ab75b371161ffe43afad8860a12edcaf34b
Ilia <shishmagaevi@mail.ru>
2023-01-20 18:26:37 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.WriteLine("Введите натуральное положительное число");
+    return;
+}
+else Quarter(number);
+
+void Quarter(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}      {count * count * count}");
+        count++;
+    }
+}
