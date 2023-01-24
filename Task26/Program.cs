7b6db9253f863dcbe0830970a9c8710835db7cab
Ilia <shishmagaevi@mail.ru>
2023-01-24 15:34:50 +0300

    task26

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = CountDigit(number);
+Console.WriteLine($"Количество цифр в числе = {result}");
+
+
+int CountDigit(int num)
+{
+int count = 0;
+if (num < 0) num = num * -1;
+
+while (num > 0)
+{
+num = num / 10;
+count ++;
+}
+return count;
+}
