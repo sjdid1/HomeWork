0a7b38a5006996dffe8b3e8d552745f000f98423
Ilia <shishmagaevi@mail.ru>
2023-01-27 18:55:06 +0300

    Task 35,37

---
+++ Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int ReturnNumberElements(int[] array, int min, int max)
+{
+    int numberElem = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max)
+        {
+            numberElem ++;
+        }
+    }
+    return numberElem;
+}
+
+int[] arr = CreateArrayRndInt(123, 0, 100);
+PrintArray(arr);
+int returnNumberElements = ReturnNumberElements(arr, 10, 99);
+Console.WriteLine($" -> {returnNumberElements}");
+
---
+++ Task37/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreateArrayRndInd(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] ArrayProductNumberFirstLast(int[] array)
+{
+int size = array.Length / 2;
+if (array.Length % 2 == 1) size += 1;
+int[] arrayNew = new int[size];
+for (int i = 0; i < size; i++)
+{
+arrayNew[i] = array[i] * array[array.Length - 1 - i];
+}
+if (array.Length % 2 == 1) arrayNew[arrayNew.Length - 1] = array[array.Length / 2];
+return arrayNew;
+}
+
+int[] arr = CreateArrayRndInd(5, 0, 10);
+PrintArray(arr);
+int[] arrNew = ArrayProductNumberFirstLast(arr);
+PrintArray(arrNew);
