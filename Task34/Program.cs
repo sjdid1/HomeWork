d342df4774b2d6e1317bc932394c5a462af59a23
Ilia <shishmagaevi@mail.ru>
2023-01-27 21:31:49 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int ParityNumbers(int[] array, int min, int max)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+int[] arrayRndInt = CreateArrayRndInt(6, 100, 999);
+PrintArray(arrayRndInt);
+int parityNumbers = ParityNumbers(arrayRndInt, 100, 999);
+Console.WriteLine($" -> {parityNumbers}");
