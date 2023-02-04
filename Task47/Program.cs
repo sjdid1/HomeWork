d8b19da33a3ac1dbd5c54b9051f027c95a193465
Ilia <shishmagaevi@mail.ru>
2023-02-04 14:27:38 +0300

    Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -5,36 +5,33 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-void CreateMatrix(double[,] matrix)
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
 {
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
         }
     }
+    return matrix;
 }
 
 void PrintMatrix(double[,] matrix)
+
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
-            else Console.Write($"{matrix[i, j],5}");
-        }
-        Console.WriteLine("|");
-        
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}, ");
+        else Console.Write($"{matrix[i, j], 5}");
+    }
+       Console.WriteLine("|");
     }
 }
-
-Console.WriteLine("введите число m(количество строк)");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число n(количество столбцов)");
-int columns = Convert.ToInt32(Console.ReadLine());
-double[,] numbers = new double[rows, columns];
-CreateMatrix(numbers);
-PrintMatrix(numbers);
+double[,] array2D = CreateMatrixRndDouble(3,4,-100,100);
+PrintMatrix(array2D);
