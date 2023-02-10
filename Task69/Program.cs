29ce8bacdae4c3197faa2b1ba9d7c1ff8c2b323f
Ilia <shishmagaevi@mail.ru>
2023-02-10 16:20:13 +0300

    task69

---
+++ Task69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int SumDigits(int num1, int num2)
+{
+    return num2 == 0 ? 1 : num1 * SumDigits(num1, num2 - 1);
+}
+
+Console.Write("Введите число A :");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B :");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int numberOneAExtentNumberTwo = SumDigits(numberOne, numberTwo);
+Console.WriteLine(numberOneAExtentNumberTwo);
